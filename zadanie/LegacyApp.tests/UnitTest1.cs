5d63de0f47804d23fb3571605ea21c23ad58942c
Karol Niekrasz <karolniekrasz@gmail.com>
2024-04-02 17:18:59 +0200

    Add new test

--- zadanie/LegacyApp.tests/UnitTest1.cs
+++ zadanie/LegacyApp.tests/UnitTest1.cs
@@ -3,26 +3,40 @@ namespace LegacyApp.tests;
 public class UnitTest1
 {
     [Fact]
-    public void GetTest()
+    public void Test1()
     {
         UserService us = new UserService();
-        us.AddUser("Jan", "Kowalski", "kowalski@wp.pl",
-            new DateTime(2002, 01, 10), 1);
-        
+        us.AddUser("Jan", "Kowalski", "kowalski@wp.pl", new DateTime(1982, 2,2), 1);
+        us.AddUser("Jan", "Kowalski", "kowalski@wp.pl", new DateTime(2014, 12,2), 1);
         Client client = new Client();
-        var id = client.ClientId;
-        var address = client.Address;
-        var mail = client.Email;
-        var addres = client.Type;
         var name = client.Name;
+        var id = client.ClientId;
+        var email = client.Email;
+        var address = client.Address;
+        ClientRepository cr = new ClientRepository();
         User user = new User();
-        var cl = user.Client;
-        var umail = user.EmailAddress;
-        var ufn = user.FirstName;
-
+        var tmpC=user.Client;
+        var tmpE = user.EmailAddress;
+        var tmpN = user.FirstName;
     }
-    
     [Fact]
-    
-    public void UserService()
+    public void Test2()
+    {
+        UserService us = new UserService();
+        us.AddUser("Jan", "Kowalski", "kowalski@wp.pl", new DateTime(2048, 2,2), 1);
+        us.AddUser("Jan", "Kowalski", "kowalskiwppl", new DateTime(1982, 2,2), 1);
+        us.AddUser(null, "Kowalski", "kowalski@wp.pl", new DateTime(1982, 2,2), 1);
+        us.AddUser("Jan", null, "kowalski@wp.pl", new DateTime(1982, 2,2), 1);
+        us.AddUser("Jan", "Kowalski", "kowalskiwp.pl", new DateTime(1982, 2,2), 1);
+        us.AddUser("Jan", "Kowalski", "kowalski@wp.pl", new DateTime(2010, 4,2), 1);
+        us.AddUser("Jan", "Kowalski", "kowalski@wp.pl", new DateTime(2010, 3,30), 1);
+    }
+        
+    [Fact]
+    public void Test3()
+    {
+        UserService us = new UserService();
+        us.AddUser("Mariusz", "Malewski", "malewski@gmail.pl", new DateTime(1974, 2,2), 2);
+        us.AddUser("John", "Smith", "smith@gmail.pl", new DateTime(1974, 2,2), 3);
+    }
 }
